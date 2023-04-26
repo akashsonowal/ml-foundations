337e4d53c67e1f5c80865e054b1a837ff5a9a279
Akash Sonowal <work.akashsonowal@gmail.com>
2023-04-26 09:24:56 +0530

    Update autobatching_for_bayesian_inference.py

--- toolkit/jax/autobatching_for_bayesian_inference.py
+++ toolkit/jax/autobatching_for_bayesian_inference.py
@@ -14,3 +14,13 @@ from jax import random
 
 import numpy as np
 import scipy as sp
+
+# Generate a fake classification dataset
+np.random.seed(100009)
+
+num_features = 10
+num_points = 100
+
+true_beta = np.random.randn(num_features).astype(jnp.float32)
+all_x = np.random.randn(num_points, num_features).astype(jnp.float32)
+y = (np.random.rand(num_points) < sp.special.expit(all_x.dot(true_beta))).astype(jnp.int32)
