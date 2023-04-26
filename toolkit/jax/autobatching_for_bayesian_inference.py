3252d050e60125aba4b163d47d1f102b1750320c
Akash Sonowal <work.akashsonowal@gmail.com>
2023-04-26 08:42:30 +0530

    Update autobatching_for_bayesian_inference.py

--- toolkit/jax/autobatching_for_bayesian_inference.py
+++ toolkit/jax/autobatching_for_bayesian_inference.py
@@ -1,1 +1,16 @@
+import functools
+import itertools
+import re
+import sys
+import time
 
+from matplotlib.pyplot import *
+
+import jax
+from jax import lax
+import jax.numpy as jnp
+import jax.scipy as jsp
+from jax import random
+
+import numpy as np
+import scipy as sp
