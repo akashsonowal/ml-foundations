b2f06dc1a2a59949768380e5ccf5269cd264709e
Akash Sonowal <work.akashsonowal@gmail.com>
2023-04-27 19:27:42 +0530

    update attn

--- ml_foundations/layers/attention.py
+++ ml_foundations/layers/attention.py
@@ -3,7 +3,7 @@ from torch import nn
 
 class Attention(nn.Module):
     def __init__(
-        self.
+        self,
         dim: int,
         num_heads: int = 8,
         qkv_bias: bool = False, 
@@ -13,6 +13,13 @@ class Attention(nn.Module):
     ) -> None:
         super().__init__()
         self.num_heads = num_heads
+        head_dim = dim // num_heads
+        self.scale = head_dim**-0.5
+
+        self.qkv = nn.Linear(dim, dim*3, bias=qkv_bias)
+        self.attn_drop = nn.Dropout(attn_drop)
+        self.proj = nn.Linear(dim, dim, bias=proj_bias)
+        self.proj_drop = nn.Dropout(proj_drop)
     
     def forward(self, x: Tensor) -> Tensor:
         pass
