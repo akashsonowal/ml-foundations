23c99a9a11975d3bd2ed8545a188495249de1fe6
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-13 07:15:45 +0530

    update

--- ml_foundations/models/deep_models/layers/graph_attention.py
+++ ml_foundations/models/deep_models/layers/graph_attention.py
@@ -2,4 +2,11 @@ import torch
 from torch import nn
 
 class GraphAttentionLayer(nn.Module):
-  pass
+  def __init__(self, in_features: int, out_features: int, n_heads: int, is_concat: bool = True, dropout: float = 0.6, leaky_relu_negative_slope: float = 0.2):
+    super().__init__()
+    self.n_heads = n_heads
+    self.is_concat = is_concat
+    
+    if is_concat:
+      assert out_features % n_heads == 0
+    
