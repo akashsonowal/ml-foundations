f30013df37938e375f7d6bab06d36c0860a0c079
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-15 19:00:27 +0530

    Update latent_diffusion.py

--- ml_foundations/models/deep_models/latent_diffusion.py
+++ ml_foundations/models/deep_models/latent_diffusion.py
@@ -3,6 +3,10 @@ from typing import List
 import torch 
 import torch.nn as nn
 
+class DiffusionWrapper(nn.Module):
+  def forward(self):
+    pass
+
 class LatentDiffusion(nn.Module):
   def device(self):
     pass
