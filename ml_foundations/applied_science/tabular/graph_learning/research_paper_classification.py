cffec1fc095dc9198a6c8df3bca90da9ab6eed3f
akashsonowal <akashsonowal.cheme@gmail.com>
2023-05-17 22:06:54 +0530

    update

--- ml_foundations/applied_science/tabular/graph_learning/research_paper_classification.py
+++ ml_foundations/applied_science/tabular/graph_learning/research_paper_classification.py
@@ -4,14 +4,34 @@ import numpy as np
 import torch
 from torch import nn
 
-from .....models.deep_models.layers import GraphAttentionLayer
+from ml_foundations.models.deep_models.layers import GraphAttentionLayer
 
 class CoraDataset:
+
   labels: torch.Tensor
   classes: Dict[str, int]
   features: torch.Tensor
   adj_mat: torch.Tensor
-        
+
   @staticmethod
   def _download():
     pass
+
+  def __init__(self, include_edges: bool = True):
+    pass
+
+class GAT():
+  def __init__(self, in_features: int, n_hidden: int, n_classes: int, n_heads: int, dropout: float):
+    super().__init__()
+    self.layer1 = GraphAttentionLayer(in_features, n_hidden, n_heads, is_concat=True, dropout=dropout)
+    self.activation = nn.ELU()
+    self.dropout = nn.Dropout(dropout)
+
+  def forward(self):
+    pass
+
+def main():
+  pass
+
+if __name__ == "__main__":
+  main()
