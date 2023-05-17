2d6bc41a46f072dc38d0c7a8095e2aec32df35ab
Akash Sonowal <work.akashsonowal@gmail.com>
2023-05-17 21:47:56 +0530

    Update research_paper_classification.py

--- ml_foundations/applied_science/tabular/graph_learning/research_paper_classification.py
+++ ml_foundations/applied_science/tabular/graph_learning/research_paper_classification.py
@@ -5,3 +5,13 @@ import torch
 from torch import nn
 
 from .....models.deep_models.layers import GraphAttentionLayer
+
+class CoraDataset:
+  labels: torch.Tensor
+  classes: Dict[str, int]
+  features: torch.Tensor
+  adj_mat: torch.Tensor
+        
+  @staticmethod
+  def _download():
+    pass
