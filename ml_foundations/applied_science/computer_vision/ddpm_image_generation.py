efff8d3769a5c8fac572849ea6516746f74fd3c7
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-08 08:52:03 +0530

    update

--- ml_foundations/applied_science/computer_vision/ddpm_image_generation.py
+++ ml_foundations/applied_science/computer_vision/ddpm_image_generation.py
@@ -5,3 +5,13 @@ import torch.utils.data
 import torchvision
 from PIL import Image 
 
+class CelebADataset(torch.utils.data.Dataset):
+    def __init__(self, img_size: int):
+        super().__init__()
+
+def main():
+    pass 
+
+if __name__ == "__main__":
+    main()
+
