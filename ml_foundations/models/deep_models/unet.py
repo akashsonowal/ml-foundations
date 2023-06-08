245458e4db1c0a2c98797212d8e361ee02e6d7e1
Akash Sonowal <work.akashsonowal@gmail.com>
2023-06-08 08:41:36 +0530

    Update unet.py

--- ml_foundations/models/deep_models/unet.py
+++ ml_foundations/models/deep_models/unet.py
@@ -1,2 +1,12 @@
+import math
+from typing import Optional, Tuple, Union, List
+
+import torch
+from torch import nn
+
+class Swish(nn.Module):
+  def forward(self):
+    pass
+
 class UNetModel(nn.Module):
   pass
