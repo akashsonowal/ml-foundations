e3ce4282cfceba4efee96dc4b9e639c3bba40150
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-23 16:24:28 +0530

    update

--- ml_foundations/models/basic_models/supervised/adaboost.py
+++ ml_foundations/models/basic_models/supervised/adaboost.py
@@ -5,7 +5,7 @@ import numpy as np
 # Decision stump used as weak classifier
 class DecisionStump: # single node tree
     def __init__(self):
-        self.polarity = 1 # rightside of threshold
+        self.polarity = 1 
         self.feature_idx = None
         self.threshold = None 
         self.alpha = None  # performance weightage
@@ -14,7 +14,7 @@ class DecisionStump: # single node tree
         n_samples = X.shape[0]
         X_column = X[:, self.feature_idx]
 
-        predictions = np.ones(n_samples) # initialize everything to right i.e., 1
+        predictions = np.ones(n_samples) 
         if self.polarity == 1:
             predictions[X_column < self.threshold] = -1
         else:
@@ -35,13 +35,62 @@ class Adaboost:
         # Iterate through the classifiers
         for _ in range(self.n_clf):
             clf = DecisionStump()
-            min_error = float("-Inf")
+            min_error = float("Inf")
 
             # greedy search to find threshold and feature
             for feature_i in range(n_features):
                 X_column = X[:, feature_i]
                 thresholds = np.unique(X_column)
-                
+
+                for threshold in thresholds:
+                    # predict with polarity 1
+                    p = 1
+                    predictions = np.ones(n_samples)
+                    predictions[X_column < threshold] = -1
+
+                    # Error is sum of weights of misclassified samples
+                    misclassified = w[y != predictions]
+                    error = sum(misclassified)
+
+                    if error > 0.5:
+                        error = 1 - error
+                        p = -1
+                    
+                    # store the best configuration for a single tree
+                    if error < min_error:
+                        clf.polarity = p 
+                        clf.threshold = threshold
+                        clf.feature_idx = feature_i
+                        min_error = error
+                    
+            # calculate alphs 
+            EPS = 1e-10
+            clf.alpha = 0.5 * log((1.0 - min_error + EPS) / (min_error + EPS))
+
+            # calculate predictions and update weights
+            predictions = clf.predict(X)
+
+            w *= np.exp(-clf.alpha * y * predictions)
+            w /= np.sum(w) # normalize to 1
+
+            # save classifier
+            self.clfs.append(clf)
+        
+    def predict(self, X):
+        clf_preds = [clf.alpha * clf.predict(X) for clf in self.clfs]
+        y_pred = np.sum(clf_preds, axis=0)
+        y_pred = np.sign(y_pred)
+        return y_pred
+
+if __name__ == "__main__":
+    from sklearn import datasets
+    from sklearn.model_selection import train_test_split 
+    
+
+
+
+
+
 
 
 
