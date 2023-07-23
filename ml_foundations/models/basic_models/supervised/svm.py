f1f0a652cb904ab3a660625ae940c861ef0847ae
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-23 18:03:29 +0530

    update

--- ml_foundations/models/basic_models/supervised/svm.py
+++ ml_foundations/models/basic_models/supervised/svm.py
@@ -37,4 +37,8 @@ if __name__ == "__main__":
     X, y = datasets.make_blobs(n_samples=50, n_features=2, centers=2, cluster_std=1.05, random_state=40)
 
     y = np.where(y==0, -1, 1)
-    
+
+    clf = SVM()
+    clf.fit(X, y)
+
+    print(clf.w,  clf.b)
