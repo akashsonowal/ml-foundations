641c4aa98641c1531879fcac734ac4d4bd4c3464
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-23 15:53:34 +0530

    update

--- ml_foundations/models/basic_models/supervised/adaboost.py
+++ ml_foundations/models/basic_models/supervised/adaboost.py
@@ -1,1 +1,35 @@
+"""Adaboost for classification"""
+
 import numpy as np 
+
+# Decision stump used as weak classifier
+class DecisionStump: # single node tree
+    def __init__(self):
+        self.polarity = 1 # rightside of threshold
+        self.feature_idx = None
+        self.threshold = None 
+        self.alpha = None  # performance weightage
+    
+    def predict(self, X):
+        n_samples = X.shape[0]
+        X_column = X[:, self.feature_idx]
+
+        predictions = np.ones(n_samples) # initialize everything to right i.e., 1
+        if self.polarity == 1:
+            predictions[X_column < self.threshold] = -1
+        else:
+            predictions[X_column > self.threshold] = -1
+        return predictions
+
+class Adaboost:
+    def __init__(self, n_clf=6):
+        self.n_clf = n_clf
+        self.clfs = []
+    
+    def fit(self, X, y):
+        n_samples, n_features = X.shape 
+        
+
+    
+
+
