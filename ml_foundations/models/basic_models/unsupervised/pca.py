d9854e4f53af2962abe4a4b3abd17953557db6e4
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-23 21:28:10 +0530

    update

---
+++ ml_foundations/models/basic_models/unsupervised/pca.py
@@ -0,0 +1,14 @@
+import numpy as np 
+
+class PCA:
+    def __init__(self, n_components):
+        self.n_components = n_components
+        self.components = None 
+        self.mean = None 
+    
+    def fit(self, X, y):
+        pass 
+    
+    def transform(self):
+        pass 
+        
