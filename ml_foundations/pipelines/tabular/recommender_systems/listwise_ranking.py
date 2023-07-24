3a889b9d8ec5febcf50d77d91de945ad14484c24
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-24 22:01:57 +0530

    update

--- ml_foundations/pipelines/tabular/recommender_systems/listwise_ranking.py
+++ ml_foundations/pipelines/tabular/recommender_systems/listwise_ranking.py
@@ -1,11 +1,17 @@
 # Implicit feedback
 # Pointwise loss
 
-# Pair wise loss
+# Pair wise loss (RankNet)
 # For each user consider a pair of items and order that pair
 # - Bayesian personalised ranking loss
 # - hinge loss
 
-# Listwise loss
+# Listwise loss (LambdaMART)
 # It optimizes the ordering of entire list of itens
 # - Normalized Discounted Cumulative Gain (NDCG)
+
+"""NDCG is used in listwise ranking"""
+
+class LambdaMART:
+    def __init__(self, )
+
