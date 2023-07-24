bb7c1ea0cc9661edb6c48710e66eed04f67983dd
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-24 08:35:11 +0530

    update

--- ml_foundations/models/basic_models/supervised/lda.py
+++ ml_foundations/models/basic_models/supervised/lda.py
@@ -17,13 +17,40 @@ class LDA:
         # SB = sum( n_c * (mean_X_c - mean_overall)^2 )
         SB = np.zeros((n_features, n_features))
 
-        mean_overall = np.mean(X, axis=0)
+        mean_overall = np.mean(X, axis=0) 
         for c in class_labels:
-            X_c = X[y == c]
-            mean_c = np.mean(X_c, axis=0)
-            
+            X_c = X[y == c] # (n_c, dim)
+            mean_c = np.mean(X_c, axis=0) # (1, dim)
+            SW += (X_c - mean_c).T.dot((X_c - mean_c)) 
 
+            n_c = X_c.shape[0]
+            mean_diff = (mean_c - mean_overall).reshape(n_features, 1) # (1, dim) -> (dim, 1)?
+            SB += n_c * (mean_diff).dot(mean_diff.T) # (dim, dim)
+        
+        # Determine SW^-1 * SB
+        A = np.linalg.inv(SW).dot(SB)
+        # Get eigenvalues and eigenvectors of SW^-1 * SB
+        eigenvalues, eigenvectors = np.linalg.eig(A) # eigenvector v = [:,i] column vector, transpose for easier calculations
+        eigenvectors = eigenvectors.T 
+        # sort eigenvalues high to low
+        idxs = np.argsort(abs(eigenvalues))[::-1] # descending order
+        eigenvalues = eigenvalues[idxs]
+        eigenvectors = eigenvectors[idxs]
+        # store first n eigenvectors
+        self.linear_discriminants = eigenvectors[0 : self.n_components]
 
-    
     def transform(self, X):
         return np.dot(X, self.linear_discriminants.T)
+
+if __name__ == "__main__":
+    from sklearn import datasets 
+    data =  datasets.load_iris()
+    X, y = data.data, data.target
+
+    lda = LDA(2)
+    lda.fit(X, y)
+
+    X_projected = lda.transform(X)
+
+    print("Shape of X:", X.shape)
+    print("Shape of transformed X:", X_projected.shape)
