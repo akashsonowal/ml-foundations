f5b1850c8e79bb5b7d4b4de9251dc27ab0990ae6
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-24 08:05:09 +0530

    update

--- ml_foundations/models/basic_models/supervised/lda.py
+++ ml_foundations/models/basic_models/supervised/lda.py
@@ -6,7 +6,24 @@ class LDA:
         self.discriminants = None 
     
     def fit(self, X, y):
-        pass 
+        n_features = X.shape[1]
+        class_labels = np.unique(y)
+
+        # Within class scatter matrix
+        # SW = sum((X_c - mean_X_c)^2 )
+        SW = np.zeros((n_features, n_features))
+
+        # Between class scatter
+        # SB = sum( n_c * (mean_X_c - mean_overall)^2 )
+        SB = np.zeros((n_features, n_features))
+
+        mean_overall = np.mean(X, axis=0)
+        for c in class_labels:
+            X_c = X[y == c]
+            mean_c = np.mean(X_c, axis=0)
+            
+
+
     
     def transform(self, X):
         return np.dot(X, self.linear_discriminants.T)
