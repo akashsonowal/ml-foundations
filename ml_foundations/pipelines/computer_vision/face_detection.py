ee9a5f14b0146132ccedeba231742c2ad2a2fc74
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-29 17:52:13 +0530

    Update face_detection.py

--- ml_foundations/pipelines/computer_vision/face_detection.py
+++ ml_foundations/pipelines/computer_vision/face_detection.py
@@ -1,2 +1,3 @@
 # Voila Jones Cascade classfier
 # https://www.mygreatlearning.com/blog/viola-jones-algorithm/
+# https://towardsdatascience.com/viola-jones-algorithm-and-haar-cascade-classifier-ee3bfb19f7d8
