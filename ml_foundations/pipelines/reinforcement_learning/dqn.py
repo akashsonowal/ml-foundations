596e41fec1a5573ad1635a3bd5497d2b9bd8326f
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-29 22:38:04 +0530

    update

--- ml_foundations/pipelines/reinforcement_learning/dqn.py
+++ ml_foundations/pipelines/reinforcement_learning/dqn.py
@@ -1,3 +1,21 @@
 # approximate q values at a state
 # https://github.com/vwxyzjn/cleanrl/blob/master/cleanrl/dqn_atari.py
 # https://github.com/higgsfield/RL-Adventure/blob/master/1.dqn.ipynb
+
+import random
+import math
+import gym
+import numpy as np  
+
+import torch 
+import torch.nn as nn
+import torch.optim as optim 
+import torch.autograd as autograd 
+import torch.nn.functional as F 
+
+from IPython.display import clear_output
+import matplotlib.pyplot as plt 
+%matplotlib inline 
+
+
+
