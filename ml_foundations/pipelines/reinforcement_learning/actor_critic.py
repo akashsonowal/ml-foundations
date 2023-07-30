5ff403686ca5ac37244685157a9f8f31d20a2c4d
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-30 14:33:03 +0530

    update

--- ml_foundations/pipelines/reinforcement_learning/actor_critic.py
+++ ml_foundations/pipelines/reinforcement_learning/actor_critic.py
@@ -1,1 +1,11 @@
-# hyvrid of value based and policy based
+# hybrid architecture (both value based and policy based)
+# Math + Code: https://towardsdatascience.com/understanding-actor-critic-methods-931b97b6df3f
+import sys 
+import gym 
+
+import torch
+import torch.nn as nn 
+
+class ActorCritic(nn.Module):
+    pass 
+
