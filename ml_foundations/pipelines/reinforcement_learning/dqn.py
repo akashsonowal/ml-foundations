0f5d26f48028c9c44e8644052bd92a2c03b5ddae
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-30 07:48:37 +0530

    update

--- ml_foundations/pipelines/reinforcement_learning/dqn.py
+++ ml_foundations/pipelines/reinforcement_learning/dqn.py
@@ -17,8 +17,23 @@ from IPython.display import clear_output
 import matplotlib.pyplot as plt 
 %matplotlib inline 
 
+from collections import dequeue
+
 class ReplayBuffer(object):
-    pass 
+    def __init__(self, capacity):
+        self.buffer = dequeue(maxlen=capacity)
+    
+    def push(self, state, action, reward, next_state, done):
+        state = np.expand_dims(state, 0)
+        next_state = np.expand_dims(next_state, 0)
+
+        self.buffer.append((state, action, reward, next_state, done))
+    
+    def sample(self, batch_size):
+        state, action, reward, next_state, done = zip(*random.sample(self.buffer, batch_size))
+    
+    def __len__(self):
+        return len(self.buffer)
 
 class DQN(nn.Module):
     def __init__(self):
@@ -47,9 +62,18 @@ def compute_td_loss(batch_size):
     return loss 
 
 if __name__ == "__main__":
+
+    USE_CUDA = torch.cuda.is_available()
+    Variable = lambda *args, **kwargs: autograd.Variable(*args, **kwargs).cuda() if USE_CUDA else autograd.Variable(*args, **kwargs)
     # cartpole environment
     env_id = "CartPole-v0"
     env = gym.make(env_id)
 
-    #
+    epsilon_start = 1.0
+    epsilon_final = 0.01
+    epsilon_decay = 500
+
+    epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx * epsilon_decay)
+
+    
 
