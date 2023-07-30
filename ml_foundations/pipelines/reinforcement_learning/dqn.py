7b068d3465d7d1bb1051c66eeb436cf0806bb1ef
Akash Sonowal <work.akashsonowal@gmail.com>
2023-07-30 08:18:06 +0530

    update

--- ml_foundations/pipelines/reinforcement_learning/dqn.py
+++ ml_foundations/pipelines/reinforcement_learning/dqn.py
@@ -62,10 +62,18 @@ class DQN(nn.Module):
 def compute_td_loss(batch_size):
     state, action, reward, next_state, done = replay_buffer.sample(batch_size)
 
+    state      = Variable(torch.FloatTensor(np.float32(state)))
+    next_state = Variable(torch.FloatTensor(np.float32(next_state)), volatile=True)
+    action     = Variable(torch.LongTensor(action))
+    reward     = Variable(torch.FloatTensor(reward))
+    done       = Variable(torch.FloatTensor(done))
+
     q_values = model(state)
     next_q_values = model(next_state)
 
-    expected_q_value = reward * gamma * next_q_value * (1 - done)
+    q_value          = q_values.gather(1, action.unsqueeze(1)).squeeze(1)
+    next_q_value     = next_q_values.max(1)[0]
+    expected_q_value = reward + gamma * next_q_value * (1 - done)
 
     loss = (q_value - Variable(expected_q_value.data)).pow(2).mean()
 
@@ -74,6 +82,17 @@ def compute_td_loss(batch_size):
     optimizer.step()
 
     return loss 
+
+def plot(frame_idx, rewards, losses):
+    clear_output(True)
+    plt.figure(figsize=(20,5))
+    plt.subplot(131)
+    plt.title('frame %s. reward: %s' % (frame_idx, np.mean(rewards[-10:])))
+    plt.plot(rewards)
+    plt.subplot(132)
+    plt.title('loss')
+    plt.plot(losses)
+    plt.show()
 
 if __name__ == "__main__":
 
@@ -90,6 +109,47 @@ if __name__ == "__main__":
     epsilon_by_frame = lambda frame_idx: epsilon_final + (epsilon_start - epsilon_final) * math.exp(-1. * frame_idx * epsilon_decay)
 
     plt.plot([epsilon_by_frame(i) for i in range(10000)])
+
+    model = DQN(env.observation_space.shape[0], env.action_space.n)
+
+    if USE_CUDA:
+        model = model.cuda()
+    
+    optimizer = optim.Adam(model.parameters())
+    replay_buffer = ReplayBuffer(1000)
+
+    num_frames = 10000
+    batch_size = 32
+    gamma      = 0.99
+
+    losses = []
+    all_rewards = []
+    episode_reward = 0
+
+    state = env.reset()
+    for frame_idx in range(1, num_frames + 1):
+        epsilon = epsilon_by_frame(frame_idx)
+        action = model.act(state, epsilon)
+        
+        next_state, reward, done, _ = env.step(action)
+        replay_buffer.push(state, action, reward, next_state, done)
+        
+        state = next_state
+        episode_reward += reward
+        
+        if done:
+            state = env.reset()
+            all_rewards.append(episode_reward)
+            episode_reward = 0
+        
+        if len(replay_buffer) > batch_size:
+            loss = compute_td_loss(batch_size)
+            losses.append(loss.data[0])
+            
+        if frame_idx % 200 == 0:
+            plot(frame_idx, all_rewards, losses)
+
+
 
 
 
