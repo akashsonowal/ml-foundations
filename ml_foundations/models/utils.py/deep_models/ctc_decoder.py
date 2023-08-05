2379ed11540e090b2bee888f9b1c948d727e1d16
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-05 18:37:31 +0530

    update

--- ml_foundations/models/utils.py/deep_models/ctc_decoder.py
+++ ml_foundations/models/utils.py/deep_models/ctc_decoder.py
@@ -13,4 +13,9 @@ if __name__ == "__main__":
 
     time = 50
     output_dim = 20
-    
+
+    probs = np.random.rand(time, output_dim)
+    probs = probs / np.sum(probs, axis=1, keepdims=True)
+
+    labels, score = ctc_decode(probs)
+    print(f"Score {score}:.3f")
