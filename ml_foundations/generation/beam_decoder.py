0c43d88d346e5f09983343c94827ad9005a75f21
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-06 18:37:24 +0530

    update

--- ml_foundations/generation/beam_decoder.py
+++ ml_foundations/generation/beam_decoder.py
@@ -1,20 +1,37 @@
-import math 
+import numpy as np
+import math
 
-def beam_search_decoder(probs, k):
+def beam_search_decoder(probs, beam_size):
     T, S = probs.shape
 
-    beams = [ [list(), 0.0] ]
+    beams = [[[], 0.0]]
 
     for t in range(T):
-        all_candidates = list()
-        for i in range(len(sequences)):
-            seq, score = sequences[i]
+        new_beam = []
 
-            for j in range(len(row)):
-                candidate = [seq + j, score - math.log(row[j])]
+        for i in range(len(beams)):
+            seq, score = beams[i]
 
-            all_candidates.append(candidate)
-    
-    ordered = sorted(all_candidates, key=lambda tup: tup[1])
-    sequences = ordered[:k]
-    return sequences
+            for j in range(S):  # Iterate over each possible next token
+                candidate_seq = seq + [j]  # Add the next token to the sequence
+                candidate_score = score - math.log(probs[t, j])  # Update the score
+
+                new_beam.append([candidate_seq, candidate_score])
+
+        # Sort and keep only top-k sequences based on the score
+        new_beam.sort(key=lambda x: x[1])
+        beams = new_beam[:beam_size]
+
+    return beams
+
+if __name__ == "__main__":
+    np.random.seed(3)
+
+    time = 50
+    output_dim = 20
+
+    probs = np.random.rand(time, output_dim)
+    probs = probs / np.sum(probs, axis=1, keepdims=True)
+
+    labels, score = beam_search_decoder(probs, 5)
+    print(f"Score {score :.3f}")
