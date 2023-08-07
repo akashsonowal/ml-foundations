20ea8c043999a431896444bb20fb3dc58c484c61
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-07 08:53:55 +0530

    update

--- ml_foundations/models/utils/deep_models/metrics/xer.py
+++ ml_foundations/models/utils/deep_models/metrics/xer.py
@@ -1,2 +1,13 @@
 import numpy as np 
 
+def levenstein_distance(ref, hyp):
+    pass 
+
+
+if __name__ == "__main__":
+    a = ['Mavs', 'Spurs', 'Lakers', 'Cavs']
+    b = ['Rockets', 'Pacers', 'Warriors', 'Celtics']
+
+    for i, k in zip(a, b):
+        print(levenstein_distance(i, k))
+
