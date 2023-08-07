346eff153e05c488e301aaf4453ddfab4a31a5c8
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-07 09:07:04 +0530

    update

--- ml_foundations/models/utils/deep_models/metrics/xer.py
+++ ml_foundations/models/utils/deep_models/metrics/xer.py
@@ -4,6 +4,11 @@ def levenstein_distance(ref, hyp): # character matching b/ew words
     m, n = len(ref), len(hyp)
     if ref == hyp:
         return 0
+    if m == 0:
+        return n 
+    if n == 0:
+        return m 
+        
 
 
 if __name__ == "__main__":
