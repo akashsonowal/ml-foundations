fa32ea4c1a8f0efa2241c187ad1c0b892b4434b7
Akash Sonowal <work.akashsonowal@gmail.com>
2023-08-08 09:40:59 +0530

    update

--- ml_foundations/models/utils/deep_models/metrics/xer.py
+++ ml_foundations/models/utils/deep_models/metrics/xer.py
@@ -1,2 +1,13 @@
 from .edit_distance import levenstein_distance
 
+def wer(source_seq, target_seq, ignore_case=True):
+    if ignore_case:
+        source_seq = source_seq.lower()
+        target_seq = target_seq.lower()
+    
+    source_words = source_seq.split(" ")
+    target_words = target_seq.split(" ")
+
+    edit_distance = levenstein_distance(source_words, target_words)
+    return float(edit_distance) / len(source_words)
+
